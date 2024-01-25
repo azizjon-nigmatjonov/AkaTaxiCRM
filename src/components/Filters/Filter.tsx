afcc634d1c02781a88e76df1511035d00ce566fb
giyos <rashidovgiyos409@gmail.com>
2024-01-25 14:51:32 +0500

    baseURL change to newapi

--- src/components/Filters/Filter.tsx
+++ src/components/Filters/Filter.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 const Filters = ({ toggle = () => {}, children }: Props) => {
   return (
-    <div className="absolute right-0 top-[50px] z-[99]">
+    <div className="absolute shadow-lg rounded-[10px] right-0 top-[50px] z-[99]">
       <div className="p-[14px] relative z-[99] w-[240px] border border-[var(--lineGray)] rounded-[10px] bg-white">
         {children}
       </div>
