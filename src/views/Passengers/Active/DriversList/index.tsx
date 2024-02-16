62dc9885725fb86ae2eeaf510ec43e363055e0d0
giyos <rashidovgiyos409@gmail.com>
2024-02-16 15:50:03 +0500

    passanger section driver_list style fixed

--- src/views/Passengers/Active/DriversList/index.tsx
+++ src/views/Passengers/Active/DriversList/index.tsx
@@ -2,7 +2,7 @@ import { useGetQueries } from '../../../../hooks/useGetQueries';
 import usePageRouter from '../../../../hooks/useObjectRouter';
 import CModal from '../../../../components/CElements/CModal'
 import Avatar from '@mui/material/Avatar';
-
+import { IoIosArrowRoundForward } from "react-icons/io";
 
 const DriversList = ({ data }: { data?: any }) => {
     const query = useGetQueries();
@@ -11,17 +11,21 @@ const DriversList = ({ data }: { data?: any }) => {
 
     return (
         <div>
-            <CModal title={query.driver_list  ? "Aktiv haydovchilar" : "Tahrirlash"}
+            <CModal title={query.driver_list ? "Aktiv haydovchilar" : "Tahrirlash"}
                 open={!!query.driver_list}
                 handleClose={() => {
                     navigateQuery({ driver_list: "" });
                 }}
-                footerActive={false} 
+                footerActive={false}
             >
-
+                <div className='flex items-center justify-between'>
+                    <p className='text-[24px] text-[var(--black)] font-semibold'>Toshkent</p>
+                    <IoIosArrowRoundForward size={24} />
+                    <p className='text-[24px] text-[var(--black)] font-semibold'>Navoiy</p>
+                </div>
                 <div>
                     {data?.map((val: any) => (
-                        <div className='flex items-center gap-3'>
+                        <div className='flex items-center gap-3 p-4 border-b border-[#EAECF0]'>
                             <Avatar sx={{ width: 24, height: 24 }} alt={val?.full_name} src={val?.image} />
                             <div>
                                 <p className='text-sm font-semibold text-[var(--black)]'>{val?.full_name}</p>
