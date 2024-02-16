4889ad0fc3bf21fc893876f6d1e41e4108665089
giyos <rashidovgiyos409@gmail.com>
2024-02-16 17:05:42 +0500

    modal height

--- src/views/Passengers/Active/DriversList/index.tsx
+++ src/views/Passengers/Active/DriversList/index.tsx
@@ -7,7 +7,6 @@ import { IoIosArrowRoundForward } from "react-icons/io";
 const DriversList = ({ data }: { data?: any }) => {
     const query = useGetQueries();
     const { navigateQuery, } = usePageRouter();
-    console.log(query.driver_list);
 
     return (
         <div>
@@ -23,7 +22,7 @@ const DriversList = ({ data }: { data?: any }) => {
                     <IoIosArrowRoundForward size={24} />
                     <p className='text-[24px] text-[var(--black)] font-semibold'>Navoiy</p>
                 </div>
-                <div>
+                <div className='h-[500px] overflow-y-scroll'>
                     {data?.map((val: any) => (
                         <div className='flex items-center gap-3 p-4 border-b border-[#EAECF0]'>
                             <Avatar sx={{ width: 24, height: 24 }} alt={val?.full_name} src={val?.image} />
@@ -33,6 +32,7 @@ const DriversList = ({ data }: { data?: any }) => {
                             </div>
                         </div>
                     ))}
+                    
                 </div>
 
             </CModal>
