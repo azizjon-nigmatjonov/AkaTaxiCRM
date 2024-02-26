ac38df85c4f29144c4c150f627c1c4fbe3b32a14
giyos <rashidovgiyos409@gmail.com>
2024-02-26 17:17:46 +0500

    a

--- src/hooks/usePaginationCount/index.ts
+++ src/hooks/usePaginationCount/index.ts
@@ -1,7 +1,6 @@
-export const usePaginationCount = (count: number, limit: number) => {
-  console.log(count);
-  console.log(limit);
-  
+export const usePaginationCount = (count: number, limit?: number) => {
+ console.log(limit);
+ 
   if (!count) return 1;
   return {count, tableCount: count};
   // return {count, tableCount: count > limit ? Math.ceil(count / limit) : 1};
