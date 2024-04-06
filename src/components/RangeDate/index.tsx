2f875021e6473cfebc4ce2d01485a4185cd492a5
giyos <rashidovgiyos409@gmail.com>
2024-04-06 17:02:37 +0500

    select multiple region

---
+++ src/components/RangeDate/index.tsx
@@ -0,0 +1,19 @@
+import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
+import { LocalizationProvider } from '@mui/x-date-pickers-pro';
+import { AdapterDayjs } from '@mui/x-date-pickers-pro/AdapterDayjs';
+import { DateRangeCalendar } from '@mui/x-date-pickers-pro/DateRangeCalendar';
+// import cls from './style.module.scss';
+
+const RangeDate = () => {
+    return (
+        <div className=''>
+            <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DemoContainer components={['DateRangeCalendar']}>
+                    <DateRangeCalendar />
+                </DemoContainer>
+            </LocalizationProvider>
+        </div>
+    )
+}
+
+export default RangeDate
