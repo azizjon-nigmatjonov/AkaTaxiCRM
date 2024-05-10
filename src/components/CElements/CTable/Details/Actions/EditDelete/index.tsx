bf79824e7466e0295a665d2eb72dcf543c5e69f6
muhammadaziz <aziz.nigmatjonov7@gmail.com>
2024-05-10 15:39:07 +0500

    fixed

--- src/components/CElements/CTable/Details/Actions/EditDelete/index.tsx
+++ src/components/CElements/CTable/Details/Actions/EditDelete/index.tsx
@@ -1,4 +1,8 @@
-import { BorderColorRounded, DeleteRounded, Visibility } from "@mui/icons-material";
+import {
+  BorderColorRounded,
+  DeleteRounded,
+  Visibility,
+} from "@mui/icons-material";
 import cls from "./style.module.scss";
 
 interface Props {
@@ -9,22 +13,25 @@ interface Props {
 
 export const TableDelete = ({
   element,
-  tableActions = () => { },
+  tableActions = () => {},
   permissions = [],
 }: Props) => {
-
-
   return (
     <div className={cls.tableDelete}>
-      <div
-        className={cls.delete}
-        onClick={() => tableActions("delete", element)}
-      >
-        <DeleteRounded style={{ color: "var(--error)" }} />
-      </div>
-      <div className={cls.edit} onClick={() => tableActions("edit", element)}>
-        <BorderColorRounded style={{ color: "var(--gray90)" }} />
-      </div>
+      {permissions.includes("delete") && (
+        <div
+          className={cls.delete}
+          onClick={() => tableActions("delete", element)}
+        >
+          <DeleteRounded style={{ color: "var(--error)" }} />
+        </div>
+      )}
+
+      {permissions.includes("edit") && (
+        <div className={cls.edit} onClick={() => tableActions("edit", element)}>
+          <BorderColorRounded style={{ color: "var(--gray90)" }} />
+        </div>
+      )}
       {permissions.includes("view") && (
         <div className={cls.edit} onClick={() => tableActions("view", element)}>
           <Visibility style={{ color: "var(--gray)" }} />
