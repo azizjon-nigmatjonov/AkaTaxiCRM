0d7fd442fa58fc337742382b2c16bf546680da3d
giyos <rashidovgiyos409@gmail.com>
2024-05-11 18:07:32 +0500

    test

--- src/views/Drivers/Drivers/Driver/Ballance/Logic/index.tsx
+++ src/views/Drivers/Drivers/Driver/Ballance/Logic/index.tsx
@@ -1,31 +1,25 @@
-import { FormatTime } from "../../../../../../utils/formatTime";
+import { FormatTime } from "../../../../../../utils/formatTime"
 
 export const headColums = [
-  {
-    title: "Summa",
-    id: "amount",
-    render: (val?: any, item?: any) =>
-      val && (
-        <p
-          className={
-            item.type == "income" ? "text-[var(--green)]" : "text-[var(--main)]"
-          }
-        >
-          {item.type == "income" ? "+" : "-" + val + " " + "so'm"}
-        </p>
-      ),
-  },
-  {
-    title: "tranzaksiya turi",
-    id: "type",
-    render: (val?: any) =>
-      val && (
-        <p>{val == "income" ? "Hisob to’ldirildi" : "Komissiya olindi"}</p>
-      ),
-  },
-  {
-    title: "Sana",
-    id: "created_at",
-    render: (val?: any) => val && FormatTime(val),
-  },
-];
+    {
+        title: 'Summa',
+        id: 'amount',
+        render: (val?: any, item?: any) => val && (
+            <p className={item.type == 'income' ? 'text-[var(--green)]' : 'text-[var(--main)]'}>{item.type == 'income' ? '+' : '-' + val + ' ' + "so'm"}</p>
+        )
+    },
+    {
+        title: 'tranzaksiya turi',
+        id: "type",
+        render: (val?: any) => val && (
+            <p >{val == 'income' ? 'Hisob to’ldirildi' : 'Komissiya olindi'}</p>
+        )
+    },
+    {
+        title: 'Sana',
+        id: 'created_at',
+        render: (val?: any) => val && (
+            FormatTime(val)
+        )
+    }
+]
