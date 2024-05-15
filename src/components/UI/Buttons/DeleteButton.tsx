de335404232b75ab78c7314e932721e36fef7bbd
muhammadaziz <aziz.nigmatjonov7@gmail.com>
2024-05-15 16:22:47 +0500

    added: delete button

---
+++ src/components/UI/Buttons/DeleteButton.tsx
@@ -0,0 +1,47 @@
+import { Button } from "@mui/material";
+import "./style.scss";
+import { PlusIcon } from "../IconGenerator/Svg";
+import { useTranslation } from "react-i18next";
+import { PermissionsData } from "../../../hooks/usePermissions";
+
+interface Props {
+  text?: string;
+  iconLeft?: any;
+  style?: any;
+  id?: string;
+  children?: any;
+  classes?: string;
+  type?: any;
+
+  onClick?: (val?: any) => void;
+}
+
+const DeleteButton = ({
+  text = "",
+  iconLeft = true,
+  id,
+  children,
+  classes,
+  type = "button",
+  ...props
+}: Props) => {
+  const { t } = useTranslation();
+  const { routePermissions } = PermissionsData()
+
+  if (!routePermissions?.includes('delete')) return ""
+ 
+  return (
+    <div id={id ? id : "addBtn"} {...props}>
+      <Button type={type} className={classes} >
+        {iconLeft === true ? <PlusIcon /> : iconLeft}
+        {children ? (
+          children
+        ) : (
+          <span className="font-[600] ml-1">{t(text)}</span>
+        )}
+      </Button>
+    </div>
+  );
+};
+
+export default DeleteButton;
